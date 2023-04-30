1107cae3904bead3b748e260cb74fe2150702079
TaisonTsukada <tukadataison1208@i.softbank.jp>
2023-04-30 17:56:53 +0900

    learn: mod

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
-fn add_i32(x: i32, y: i32) -> i32 {
-    x + y
-}
-
-#[test]
-fn test1() {
-    assert_eq!(add_i32(1, 2), 3)
+mod module_hello {
+    pub fn print_hello() {
+        println!("Hello, world!");
+    }
 }
 
 fn main() {
-    println!("{}", add_i32(2, 5))
+    module_hello::print_hello();
 }
